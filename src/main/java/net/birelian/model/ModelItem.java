8cebd21c1061e7c05062257658e5fdce0c0c4990
Raúl Villalba Medina <raul.villalba@sm2baleares.es>
2016-11-07 14:26:15 +0100

    Add Lombok.
    Delete all getters/setters, toString and constructors, now autogenerated in compile.

--- src/main/java/net/birelian/model/ModelItem.java
+++ src/main/java/net/birelian/model/ModelItem.java
@@ -2,13 +2,6 @@ package net.birelian.model;
 
 import java.io.Serializable;
 
-import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-
 abstract class ModelItem implements Serializable {
 
-	@Override
-	public String toString() {
-		return ReflectionToStringBuilder.toString(this, ToStringStyle.JSON_STYLE);
-	}
 }
