b3f014f0b868566f7709b15ff78bcabceef0c5d5
Guillermo Bauza <birelian@gmail.com>
2020-05-12 12:44:20 +0200

    Add non tested class

---
+++ src/main/java/net/birelian/service/AnotherComponent.java
@@ -0,0 +1,12 @@
+package net.birelian.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnotherComponent {
+
+    public String sayHello() {
+
+        return "Hello";
+    }
+}
